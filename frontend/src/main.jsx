b9ac460510a2ff7d37f2c2b166ab548fa675270d
Aniket Bagul <167109182+aniketbagul242@users.noreply.github.com>
2025-11-20 22:32:42 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Dashboard from './pages/Dashboard';
 import Stats from './pages/Stats';
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}>
         <Route index element={<Dashboard />} />
         <Route path="/code/:code" element={<Stats />} />
       </Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 );
